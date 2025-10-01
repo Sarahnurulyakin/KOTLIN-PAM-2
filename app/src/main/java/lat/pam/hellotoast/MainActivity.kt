64b3b48446397ad9cd11152397bfdf974d00135e
Sarah Nurul Yakin <sarahnuruly@gmail.com>
2025-10-01 19:03:59 +0700

    initial commit

--- app/src/main/java/lat/pam/hellotoast/MainActivity.kt
+++ app/src/main/java/lat/pam/hellotoast/MainActivity.kt
@@ -1,7 +1,7 @@
 package lat.pam.hellotoast
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -9,30 +9,70 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.net.Uri
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     private var mCount = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        setContentView(R.layout.activity_main)
-
 
         val mShowCount = findViewById<TextView>(R.id.show_count)
         val buttonCountUp = findViewById<Button>(R.id.button_count)
         val buttonToast = findViewById<Button>(R.id.button_toast)
+        val buttonSwitchPage = findViewById<Button>(R.id.button_switchpage)
+        val buttonBrowser = findViewById<Button>(R.id.button_browser)
+        val buttonContact = findViewById<Button>(R.id.button_contact)
+        val buttonMaps = findViewById<Button>(R.id.button_maps)
+        val buttonCall = findViewById<Button>(R.id.button_call)
 
-        buttonCountUp.setOnClickListener(View.OnClickListener {
-            mCount++;
-            if (mShowCount != null)
-                mShowCount.text = mCount.toString()
+        // Counter naik
+        buttonCountUp.setOnClickListener {
+            mCount++
+            mShowCount.text = mCount.toString()
+        }
+
+        // Toast
+        buttonToast.setOnClickListener {
+            val tulisan = mShowCount.text.toString()
+            Toast.makeText(this, "Angka yang dimunculkan: $tulisan", Toast.LENGTH_LONG).show()
+        }
+
+        // Pindah halaman
+        buttonSwitchPage.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+
+        // Implicit Intent: Browser
+        buttonBrowser.setOnClickListener(View.OnClickListener {
+            val intentbrowse = Intent(Intent.ACTION_VIEW)
+            intentbrowse.setData(Uri.parse("https://www.google.com/"))
+            startActivity(intentbrowse)
         })
-        buttonToast.setOnClickListener(View.OnClickListener {
-            val tulisan: String = mShowCount?.text.toString()
-            val toast: Toast = Toast.makeText(this, "Angka yang dimunculkan "+tulisan, Toast.LENGTH_LONG)
-            toast.show()
+
+        // Implicit Intent: Kontak
+        buttonContact.setOnClickListener(View.OnClickListener {
+            val intentContact = Intent(Intent.ACTION_PICK, Uri.parse("content://contacts/people/"))
+            startActivity(intentContact)
+        })
+
+        // Implicit Intent: Maps
+        buttonMaps.setOnClickListener(View.OnClickListener {
+            val gmmIntentUri = Uri.parse("geo:-6.917464,107.619123?q=Bandung")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            startActivity(mapIntent)
+        })
+
+        // Implicit Intent: Telepon
+        buttonCall.setOnClickListener(View.OnClickListener {
+            val callIntent = Intent(Intent.ACTION_DIAL)
+            callIntent.data = Uri.parse("tel:085161076913")
+            startActivity(callIntent)
         })
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
